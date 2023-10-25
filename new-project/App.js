1529aca1e6959ffe2bc3d16ad9f9b248b63fc9ab
Carlos Bolzanell <carlosbolzanell@gmail.com>
2023-10-24 21:52:46 -0300

    explicação finalizada

--- new-project/App.js
+++ new-project/App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import HomeScreen from './src/HomeScreen';
+import UserData from './src/UserData';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="UserData" component={UserData} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ new-project/src/HomeScreen/HomeScreen.js
@@ -0,0 +1,39 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import metadata from "../storage.metadata.json";
+import { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
+import { useIsFocused } from "@react-navigation/native";
+
+
+const HomeScreen = ({ navigation }) => {
+    const [name, setName] = useState("");
+    useEffect(() => { getUserName() }, [])
+
+    const focus = useIsFocused();
+    useEffect(()=> {getUserName()}, [focus]);
+
+    const getUserName = async () => {
+        const userName = await AsyncStorage.getItem(metadata.USER.USERNAME);
+        if (userName) {
+            setName(userName);
+        }
+    }
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            {name &&
+                <View>
+                    <Text>
+                        Olá usuário {name}
+                    </Text>
+                </View>
+            }
+            <Button
+                title="Go to User Data"
+                onPress={() => navigation.navigate("UserData")}
+            />
+        </View>
+    );
+}
+
+export default HomeScreen;
